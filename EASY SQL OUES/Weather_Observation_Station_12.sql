b0eea4a0a5198bb75aa0f7bd3030b6c023b04c6c
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-08-01 10:52:22 +0530

    Weather Observation Station 12

---
+++ EASY SQL OUES/Weather_Observation_Station_12.sql
@@ -0,0 +1,6 @@
+SELECT distinct city
+FROM station 
+WHERE 
+NOT(city like 'a%' or city like 'e%' or city like 'i%' or city like 'o%' or city like 'u%')
+AND
+NOT(city like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u'  );
